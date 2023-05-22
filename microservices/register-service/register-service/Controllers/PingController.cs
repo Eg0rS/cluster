cea598e7e1837209337761c323a8a8b763e00723
Eg0rS <Ehrgit@yandex.ru>
2023-05-22 17:32:24 +0300

    init

---
+++ microservices/register-service/register-service/Controllers/PingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace register_service.Controllers;
+
+[ApiController, Route("[controller]")]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok();
+    }
+}
---
+++ microservices/user-service/user-service/Controllers/PingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace user_service.Controllers;
+
+[ApiController, Route("[controller]")]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok();
+    }
+}
