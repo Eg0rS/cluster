a8a146dc321626aacc102551495012f9a89fc69b
Eg0rS <Ehrgit@yandex.ru>
2023-05-22 18:13:12 +0300

    add

---
+++ microservices/api-gateway/api-gateway/Controllers/PingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_gateway.Controllers;
+
+[ApiController, Route("[controller]")]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok();
+    }
+}
