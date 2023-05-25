c85bb706aee58fd5028f37ea11036224f69de4fe
Mya <k-shaipov@mail.ru>
2023-05-26 02:23:59 +0300

    fix config

--- microservices/personnel-service/config/config.go
+++ microservices/personnel-service/config/config.go
@@ -9,16 +9,22 @@ import (
 
 func Load(logger *zap.SugaredLogger) Settings {
 	var settings Settings
-	filePath := fmt.Sprintf("./.config/%s.json", "local")
-	fileBytes, err := os.ReadFile(filePath)
+	env, exists := os.LookupEnv("env")
 
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
+	if exists {
+		filePath := fmt.Sprintf("./.config/%s.json", env)
+		fileBytes, err := os.ReadFile(filePath)
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
 
-	if err := json.Unmarshal(fileBytes, &settings); err != nil {
-		logger.Errorf("Unmarshalling error: %s", err)
+		if err := json.Unmarshal(fileBytes, &settings); err != nil {
+			logger.Errorf("Unmarshalling error: %s", err)
+			os.Exit(1)
+		}
+	} else {
+		logger.Error("env is not exists")
 		os.Exit(1)
 	}
 
