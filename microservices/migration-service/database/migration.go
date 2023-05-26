e824f4a10ee8b0838914dd238d131bd1e2e68cad
Mya <k-shaipov@mail.ru>
2023-05-27 00:02:12 +0300

    swagger fix

--- microservices/migration-service/database/migration.go
+++ microservices/migration-service/database/migration.go
@@ -8,3 +8,7 @@ import (
 func UpMigrations(db *sqlx.DB) error {
 	return goose.Up(db.DB, "./database/migrations/")
 }
+
+func ResetMigrations(db *sqlx.DB) error {
+	return goose.Reset(db.DB, "./database/migrations/")
+}
