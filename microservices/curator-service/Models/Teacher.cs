8067d970dd0887980cb99d43f64a895ed5acfced
blust <57102061+blustofficial@users.noreply.github.com>
2023-05-28 15:09:38 +0300

    Update Teacher.cs

--- microservices/curator-service/Models/Teacher.cs
+++ microservices/curator-service/Models/Teacher.cs
@@ -1,15 +1,12 @@
-﻿namespace curator_service.Models
-using System;
-
-public class Teacher
+namespace curator_service.Models
 {
-	public Teacher()
-	{
-		public string Surname { get; set; }
-		public string Name { get; set; }
-		public string Patronymic { get; set; }
-		public bool IsCompletedSchool { get; set; }
-		public bool HasSertificate { get; set; }
-		public int Rating { get; set; }
-}
+    public class Teacher
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public bool IsCompletedSchool { get; set; }
+        public bool HasSertificate { get; set; }
+        public int Rating { get; set; }
+    }
 }
